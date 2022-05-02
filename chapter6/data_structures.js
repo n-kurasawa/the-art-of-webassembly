6538124b47b7aa8ed41b0d5b2217cbd87ee050ab
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2022-05-02 22:29:37 +0900

    add

--- chapter6/data_structures.js
+++ chapter6/data_structures.js
@@ -1,3 +1,6 @@
 const colors = require("colors");
 const fs = require("fs");
 const bytes = fs.readFileSync(__dirname + "/data_structures.wasm");
+
+const memory = new WebAssembly.Memory({ initial: 1 });
+const mem_i32 = new Uint32Array(memory.buffer);
