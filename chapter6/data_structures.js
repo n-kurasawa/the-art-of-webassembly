9ae4e26754f977dac120f52589f3a157f3c65330
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2022-05-11 17:59:06 +0900

    wip

--- chapter6/data_structures.js
+++ chapter6/data_structures.js
@@ -4,3 +4,12 @@ const bytes = fs.readFileSync(__dirname + "/data_structures.wasm");
 
 const memory = new WebAssembly.Memory({ initial: 1 });
 const mem_i32 = new Uint32Array(memory.buffer);
+
+const obj_base_addr = 0;
+const obj_count = 32;
+const obj_stride = 16;
+
+const x_offset = 0;
+const y_offset = 4;
+const radius_offset = 8;
+const collision_offset = 12;
